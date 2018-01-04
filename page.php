aea8981092529ae71afafdc48e591e75d81b9640
Aman Manglik <amanmanglik@gmail.com>
2018-01-03 22:05:08 -0800

    Added page.php for handling general pages

---
+++ page.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+    
+    
+
+<div id="page_about_container" class="d-flex justify-content-center">
+    
+    <div id="page_about_wrapper" class="flex-column ">
+        <?php the_post(); ?>
+        <div id="page_about_title" class="d-flex">
+            <?php echo get_the_title(); ?>
+        </div>
+
+        <div id="page_about_content" class="d-flex flex-column">
+            <?php echo get_the_content(); ?>
+        </div>
+    </div>
+    
+</div>
+        
+        
+    
+    
+<?php get_footer(); ?>
+    
