f402f43b19a4668cf1cee0b2e24a9e98c7899a65
Sathyajith Bhat <sathya@sathyasays.com>
2022-09-11 16:18:00 +1000

    update archives page (#24)

--- page-archives.php
+++ page-archives.php
@@ -420,8 +420,8 @@ get_header();?>
                     <div id="archives_page_title"><?php the_title();?></div>
                     <div id="archives_page_bcb_selector_buttons">
                         <ul id="archives_page_bcb_selector">
-                            <!--<li class="archive_selected"><a class="archives_current" data-contentdiv="archive_1863">BCB 2021 Virtual (Past Sessions)</a></li>-->
-                            <li class="archive_selected"><a class="archives_current" data-contentdiv="archive_1759">BCB Spring 2019</a></li>
+                            <li class="archive_selected"><a class="archives_current" data-contentdiv="archive_1863">BCB 2021 Virtual</a></li>
+                            <li><a data-contentdiv="archive_1759">BCB Spring 2019</a></li>
                             <li><a data-contentdiv="archive_1691">BCB Spring 2018</a></li>
                             <li><a data-contentdiv="archive_1556">BCB Spring 2016</a></li>
                             <li><a data-contentdiv="archive_1458">BCB Monsoon 2015</a></li>
@@ -445,7 +445,7 @@ get_header();?>
             <?php
 $techlash_categories = array(639, 787, 933, 1065, 1232, 1465);
 $archive_categories = array(
-//    1863 => "BCB 2021 Virtual",
+    1863 => "BCB 2021 Virtual",
     1759 => "BCB Spring 2019", 1691 => "BCB Spring 2018", 
     1556 => "BCB Spring 2016", 1458 => "BCB Monsoon 2015", 1366 => 'bcbxah',
     1224 => 'bcb17', 1057 => 'bcb16', 931 => 'bcb15',
