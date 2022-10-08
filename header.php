895ed8b9cc9dd5cac759624062281f86b2a497ba
Bibhas <bd@nuiteq.com>
2022-10-08 23:29:54 +0530

    Updated favicons

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
 <title><?php echo wp_title('»', false, 'right') . " Barcamp Bangalore"; ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png?v=PY4NNGXQPr">
-<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png?v=PY4NNGXQPr">
-<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png?v=PY4NNGXQPr">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url'); ?>/images/favicon-32x32.png?v=PY4NNGXQPr">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_url'); ?>/images/favicon-16x16.png?v=PY4NNGXQPr">
 <link rel="manifest" href="manifest.json?v=PY4NNGXQPr">
 <link rel="mask-icon" href="safari-pinned-tab.svg?v=PY4NNGXQPr" color="#5bbad5">
 <link rel="shortcut icon" href="favicon.ico?v=PY4NNGXQPr">
