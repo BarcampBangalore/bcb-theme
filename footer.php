74980f8ecbb02400c27b875ce92f06238d8de4bb
Bibhas <bd@nuiteq.com>
2022-10-08 23:25:06 +0530

    Fixed more stylings

--- footer.php
+++ footer.php
@@ -7,33 +7,33 @@
       <div class="col-6 col-md">
         <h5>Reach us</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" target="_blank" href="https://discord.gg/SqYxgd45wD">Discord</a></li>
-          <li><a class="text-muted" target="_blank" href="https://twitter.com/barcampbng">Twitter</a></li>
-          <li><a class="text-muted" target="_blank" href="https://www.facebook.com/barcampbng">Facebook</a></li>
-          <li><a class="text-muted" target="_blank" href="https://www.meetup.com/Barcamp-Bangalore/">Meetup</a></li>
-          <li><a class="text-muted" target="_blank" href="mailto:contact@barcampbangalore.com">Mail us</a></li>
+          <li><a target="_blank" href="https://discord.gg/SqYxgd45wD">Discord</a></li>
+          <li><a target="_blank" href="https://twitter.com/barcampbng">Twitter</a></li>
+          <li><a target="_blank" href="https://www.facebook.com/barcampbng">Facebook</a></li>
+          <li><a target="_blank" href="https://www.meetup.com/Barcamp-Bangalore/">Meetup</a></li>
+          <li><a target="_blank" href="mailto:contact@barcampbangalore.com">Mail us</a></li>
         </ul>
       </div>
       <div class="col-6 col-md">
         <h5>Resources</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" target="_blank" href="https://planning.barcampbangalore.com/">Blog</a></li>
-          <li><a class="text-muted" target="_blank" href="https://github.com/barcampbangalore">Our code</a></li>
-          <li><a class="text-muted" target="_blank" href="https://planning.barcampbangalore.com/call-for-sponsors-for-barcamp-bangalore-2022/">Be a sponsor</a></li>
+          <li><a target="_blank" href="https://planning.barcampbangalore.com/">Blog</a></li>
+          <li><a target="_blank" href="https://github.com/barcampbangalore">Our code</a></li>
+          <li><a target="_blank" href="https://planning.barcampbangalore.com/call-for-sponsors-for-barcamp-bangalore-2022/">Be a sponsor</a></li>
         </ul>
       </div>
       <div class="col-6 col-md">
         <h5>About</h5>
         <ul class="list-unstyled text-small">
-          <li><a class="text-muted" target="_blank" href="/about">About BCB</a></li>
-          <li><a class="text-muted" target="_blank" href="/faq">FAQ</a></li>
-          <li><a class="text-muted" target="_blank" href="https://planning.barcampbangalore.com/meet-the-team/">Team behind BCB</a></li>
+          <li><a target="_blank" href="/about">About BCB</a></li>
+          <li><a target="_blank" href="/faq">FAQ</a></li>
+          <li><a target="_blank" href="https://planning.barcampbangalore.com/meet-the-team/">Team behind BCB</a></li>
         </ul>
       </div>
     </div>
     <div class="row">
       <div class="col-12 col-md">
-        <img class="mb-2" src="<?php bloginfo('template_url'); ?>/images/BCB-LOGO-02.png" alt="" height="24">
+        <img class="mb-2" src="<?php bloginfo('template_url'); ?>/images/logo-bcb22.png" alt="" height="24">
         <small class="d-block mb-3 text-muted">
           <!--[if lte IE 8]><span style="filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=2); display: inline-block;"><![endif]-->
           <span style="-webkit-transform: rotate(180deg); -moz-transform: rotate(180deg); -o-transform: rotate(180deg); -khtml-transform: rotate(180deg); -ms-transform: rotate(180deg); transform: rotate(180deg); display: inline-block;">
--- page-home.php
+++ page-home.php
@@ -54,7 +54,7 @@
 
   <div class="row featurette align-items-center first-featurette">
     <div class="col-md-7">
-      <h2 class="featurette-heading">What is BarCamp Bangalore? <span class="text-muted">#bcb2022</span></h2>
+      <h2 class="featurette-heading">What is BarCamp Bangalore? <br/><span class="text-muted">#bcb2022</span></h2>
       <p class="lead">BarCamp Bangalore is an unconference – an ad-hoc gathering born from the desire for people to share and learn in an open environment.</p>
       <p class="lead">BarCamp Bangalore is an opportunity for the Bangalore community to come together every year to learn, network, and discuss great ideas in person.</p>
       <p class="lead">We're coming back on <span id="page_home_hero_date">November 5, 2022, Saturday</span>, at the <a href="https://goo.gl/maps/m7ARhpVeEHAWxYYGA" target="_blank">RV University, Bangalore</a>.</p>
@@ -88,7 +88,7 @@
         </div>
       </div>
     </div>
-    <div class="col-md-5">
+    <div class="col-md-5 mt-4">
     <img height="500px" src="<?php bloginfo('template_url');?>/images/hero-02.jpg" />
     </div>
   </div>
@@ -97,8 +97,17 @@
 
   <div class="row featurette">
     <div class="col-md-7 order-md-2">
-      <h2 class="featurette-heading">What is an Unconference? <span class="text-muted">Come and see for yourself.</span></h2>
-      <p class="lead">Sessions are given on a variety of topics, and the day is created collaboratively – built by you and the other attendees who have a topic, story, knowledge, or experience to share with the rest of the community.</p>
+      <h2 class="featurette-heading">
+        What is an Unconference?
+        <br/><span class="text-muted">
+          You decide what to talk about and what talks to listen to. You're in control.
+        </span>
+      </h2>
+      <p class="lead">
+        Sessions are given on a variety of topics, and the day is created collaboratively – built by you
+        and the other attendees who have a topic, story, knowledge, or experience to share with the rest of
+        the community.
+      </p>
     </div>
     <div class="col-md-5 order-md-1">
     <img height="500px" src="<?php bloginfo('template_url');?>/images/hero-01.jpg" />
@@ -109,8 +118,15 @@
 
   <div class="row featurette">
     <div class="col-md-7">
-      <h2 class="featurette-heading">Meet the most outside-the-box thinkers in the city. <span class="text-muted">No, really.</span></h2>
-      <p class="lead">Sessions include topics ranging from design, technology, creativity, to entrepreneurship and lifestyle.</p>
+      <h2 class="featurette-heading">
+        Meet the most outside-the-box thinkers in the city. <br/>
+        <span class="text-muted">No, really.</span>
+      </h2>
+      <p class="lead">
+        Sessions include topics ranging from design, technology, creativity, to entrepreneurship and lifestyle.
+        If you're doing something that crosses boundaries, steps on toes, improves someone's lives, or just spreads
+        joy, we want to hear about it.
+      </p>
     </div>
     <div class="col-md-5">
     <img height="500px" src="<?php bloginfo('template_url');?>/images/hero-03.jpg" />
@@ -121,8 +137,12 @@
 
   <div class="row">
     <div class="col-lg-12 text-center mb-10">
-      <h1 class="pb-2">Event venue</h1>
+      <h1 class="pb-2 featurette-heading">Event venue</h1>
       <p class="pb-2">RV University, RV Vidyanikethan Post 8th Mile, Mysore Rd, Bengaluru, Karnataka 560059</p>
+      <div class="pb-4">
+        <h2>How to reach</h2>
+        <p>Take the metro and get down at Pattanagere station on the Purple line, towards Kengeri. The venue is right beside the metro station.</p>
+      </div>
     </div>
     <div class="col-lg-12">
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.757290776476!2d77.49752121548372!3d12.923314990887569!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bafad1d6f18a4b9%3A0xc6082c1a24eba0bf!2sRV%20University!5e0!3m2!1sen!2sin!4v1663609167948!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
@@ -133,7 +153,7 @@
 
   <div class="row text-center">
     <div class="col-lg-12 mb-10">
-      <h1 class="pb-5">Sponsors</h1>
+      <h1 class="pb-5 featurette-heading">Sponsors</h1>
     </div>
     <div class="col-lg-4">
       <h2>Friends of Barcamp </h2>
