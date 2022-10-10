3193d0e3d42aaa30dee335690ff35e4801adbbdd
Bibhas <bd@nuiteq.com>
2022-10-10 08:15:04 +0530

    Content fixes

--- page-home.php
+++ page-home.php
@@ -1,54 +1,4 @@
 <?php get_header();?>
-<!-- <div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class=""></li>
-    <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-    <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item">
-      <img src="<?php bloginfo('template_url');?>/images/header-1.jpg" alt="" height="100%">
-
-      <div class="container">
-        <div class="carousel-caption text-left">
-          <h1>Creative thinking</h1>
-          <p>Barcamp talks are usually about deeper meaning of things</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a></p>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item active carousel-item-left">
-    <img src="<?php bloginfo('template_url');?>/images/header-2.jpg" alt="" height="100%">
-
-      <div class="container">
-        <div class="carousel-caption">
-          <h1>Not your usual conference</h1>
-          <p>Some representative placeholder content for the second slide of the carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
-        </div>
-      </div>
-    </div>
-    <div class="carousel-item carousel-item-next carousel-item-left">
-    <img src="<?php bloginfo('template_url');?>/images/header-3.jpg" alt="" height="100%">
-
-      <div class="container">
-        <div class="carousel-caption text-right">
-          <h1>Don't just listen, talk</h1>
-          <p>Some representative placeholder content for the third slide of this carousel.</p>
-          <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-target="#myCarousel" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-target="#myCarousel" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </button>
-</div> -->
 <div class="container marketing">
   <!-- START THE FEATURETTES -->
 
@@ -56,14 +6,14 @@
     <div class="col-md-7">
       <h2 class="featurette-heading">What is BarCamp Bangalore? <br/><span class="text-muted">#bcb2022</span></h2>
       <p class="lead">BarCamp Bangalore is an unconference – an ad-hoc gathering born from the desire for people to share and learn in an open environment.</p>
-      <p class="lead">BarCamp Bangalore is an opportunity for the Bangalore community to come together every year to learn, network, and discuss great ideas in person.</p>
+      <p class="lead">It is an opportunity for people to come together to learn, network, and discuss great ideas in person.</p>
       <p class="lead">We're coming back on <span id="page_home_hero_date">November 5, 2022, Saturday</span>, at the <a href="https://goo.gl/maps/m7ARhpVeEHAWxYYGA" target="_blank">RV University, Bangalore</a>.</p>
       <div class="calendar-options">
         <script src="https://cdn.jsdelivr.net/npm/add-to-calendar-button@1" async defer></script>
         <div class="atcb" >
             {
                 "name":"Barcamp Bangalore",
-                "description":"BarCamp Bangalore is an opportunity for the Bangalore community to come together every year to learn, network, and discuss great ideas in person.",
+                "description":"BarCamp Bangalore is an opportunity for people to come together to learn, network, and discuss great ideas in person.",
                 "startDate":"2022-11-05",
                 "endDate":"2022-11-05",
                 "startTime":"09:00",
@@ -98,13 +48,14 @@
   <div class="row featurette">
     <div class="col-md-7 order-md-2">
       <h2 class="featurette-heading">
-        What is an Unconference?
+        What is an unconference?
         <br/><span class="text-muted">
           You decide what to talk about and what talks to listen to. You're in control.
         </span>
       </h2>
       <p class="lead">
-        Sessions are given on a variety of topics, and the day is created collaboratively – built by you
+        Unconferences do not have speaker list or a fixed schedule until the actual event date. The
+        discussions are around a variety of topics, and the day is created collaboratively – built by you
         and the other attendees who have a topic, story, knowledge, or experience to share with the rest of
         the community.
       </p>
@@ -123,7 +74,7 @@
         <span class="text-muted">No, really.</span>
       </h2>
       <p class="lead">
-        Sessions include topics ranging from design, technology, creativity, to entrepreneurship and lifestyle.
+        Sessions include topics ranging from design, technology, creativity, to entrepreneurship, lifestyle, astronomy, music, and much more!.
         If you're doing something that crosses boundaries, steps on toes, improves someone's lives, or just spreads
         joy, we want to hear about it.
       </p>
