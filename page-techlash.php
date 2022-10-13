08556461b90bf41297de967b8e0a7b990ba4af53
Bibhas <bd@nuiteq.com>
2022-10-13 19:16:18 +0530

    Fixed some content of techlash page

--- page-techlash.php
+++ page-techlash.php
@@ -10,13 +10,11 @@
 
         <div class="row">
             <div id="page_techlash_description" class="col">
-                <p><font size="3">Represent your company in the latest edition of Barcamp Bangalore Techlash.</font></p><font size="3">
+                <p><font size="3">Showcase your tech project in the latest edition of Barcamp Bangalore Techlash.</font></p><font size="3">
                 <p>
                     Talk about your latest tech smartness in front of hundreds of techies.</p>
                 <p style="font-weight: bold; font-size: 20px;">6 Min per session and a total of 10 speakers.</p>
-                <!--
-                <p>To add your techlash session <a href="https://barcampbangalore.com/bcb/add-a-session"> click here.</a></p><br><br>
-                -->
+
                 </font>
                 <p>
                     <font size="3">
@@ -28,31 +26,46 @@
         </div>
 
         <div class="row">
-            <h2 id="page_techlash_sessionheading" class="col">Techlash sessions for BCB Monsoon 2019</h2>
+            <h2 id="page_techlash_sessionheading" class="col">Techlash sessions for BCB 2022</h2>
         </div>
 
         <div id="page_techlash_post_parent" class="row">
             <?php
             $args = 'cat=1882';
             query_posts($args);
-            while (have_posts()) : the_post();
-                ?>
+            if ( have_posts() ) :
+              while (have_posts()) : the_post();
+                  ?>
 
-            <div class="col-12 col-md-6">
-                <div id="page_techlash_card" class="">
-                    <div class="page_techlash_postname">
-                        <a href="<?php echo get_permalink(); ?>">
-                            <?php the_title(); ?>
-                        </a>
-                    </div>
-                    <div class="page_techlash_author">
-                        <?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('user_nicename') . '</a>'; ?>
+                <div class="col-md-6 sessions-page-card" data-track-id="<?php echo $track_id; ?>">
+                    <div class="card archive-session-card p-3 mb-2">
+                        <div class="d-flex justify-content-between">
+                            <div class="d-flex flex-row align-items-center">
+                                <div class="icon"><?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_avatar(get_the_author_meta('ID'), 64) . '</a>'; ?></div>
+                                <div class="ms-2 c-details">
+                                    <h6 class="mb-0"><?php echo '<a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_the_author_meta('display_name') . '</a>'; ?></h6>
+                                </div>
+                            </div>
+                            <!-- <div class="badge"> <span>Design</span> </div> -->
+                        </div>
+                        <div class="mt-2">
+                            <h5 class="heading"><a href="<?php echo get_permalink(); ?>" title=""><?php echo get_the_title(); ?></a></h5>
+                            <div class="mt-2">
+                                <div class="mt-2">
+                                <span class="card-meta"><?php echo attending_users_count(get_the_ID()) ?> interested</span>,
+                                <span class="card-meta"><?php comments_number('0', '1', '%');?> comments</span> </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-
-
-            <?php endwhile; ?>
+                <?php
+              endwhile;
+            else :
+              ?>
+              <div class="col-12">Sorry no techlash session has been posted yet. <a href="<?php echo home_url() ?>/add-a-session">Showcase your project?</a></div>
+              <?php
+            endif;
+          ?>
 
         </div>
 
