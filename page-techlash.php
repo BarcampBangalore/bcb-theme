9f31b004dbcc9a1d93c1a3f97121970c7f7c65e3
Shreyas <shreyass.ranganatha@gmail.com>
2024-05-17 20:10:26 +0530

    Update Techlash CAT ID (#43)
    Signed-off-by: Shreyas Ranganatha <shreyass.ranganatha@gmail.com>

--- page-techlash.php
+++ page-techlash.php
@@ -28,7 +28,7 @@
 
         <div id="page_techlash_post_parent" class="row">
             <?php
-            $args = 'cat=1929';
+            $args = 'cat=1979';
             query_posts($args);
             if ( have_posts() ) :
               while (have_posts()) : the_post();
