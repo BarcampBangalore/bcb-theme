198f2b967cb51df70228ea376b5e6c8e5ff28cab
Diplo2by <contactdarshanv@gmail.com>
2025-01-25 12:33:15 +0530

    About section typofix

--- page-home.php
+++ page-home.php
@@ -156,7 +156,7 @@
       </h3>
       <p class="lead">
         Need feedback on something? Or looking for opinion from everyone in the room? Share your knowledge and gether
-        more from others around you. This session type os perfect to get everyone talking.
+        more from others around you. This session type is perfect to get everyone talking.
       </p>
     </div>
     <div class="col-md-4">
